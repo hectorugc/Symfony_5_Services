4ca8d1072699ef2565c485bedcb6b6a3843a37fd
hector <hectorugc4@gmail.com>
2021-01-11 22:34:01 +0100

    Learnig abour services with symfony

---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Service;
+use  Symfony\Contracts\Cache\CacheInterface;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use  Psr\Log\LoggerInterface;
+class MarkdownHelper{
+    private $markdownParser;
+    private $cache;
+    private $isDebug;
+    private $logger;
+    public function __construct(MarkdownParserInterface $markdownParser,CacheInterface $cache,bool $isDebug,LoggerInterface $markdownLogger){
+      
+        $this->markdownParser = $markdownParser;
+        $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        $this->logger = $markdownLogger;    
+        //dump($isDebug);
+    }
+    public function parse(string $source):string{
+        if (stripos($source,'cat')!== false) {
+            $this->logger->info('mewo');
+        }
+        if (!$this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
+        return  $this->cache->get('markdown_'.md5($source),function()use($source){
+            return $this->markdownParser->transformMarkdown($source);
+        });
+    }
+}
